bae6484043a6204833f9e61f5fb0f656a8989b36
HGP22 <hgp22@Thes-MacBook-Air-2.local>
2021-10-08 18:10:25 -0700

    Commitng Genius Norms

---
+++ myProjects/src/myProjects/geniusJokes.java
@@ -0,0 +1,10 @@
+package myProjects;
+
+public class geniusJokes {
+	public static void main(String[] args) {
+	    System.out.println("Welcome, My name is Ajani");//Print my Welcome Message & Name
+	    System.out.println("What do you call a fake pasta?");//Print Joke's Setup
+	    System.out.println("An Impasta");//Joke Punch Line
+	  }
+	
+ }
---
+++ myProjects/src/myProjects/geniusNorms.java
@@ -0,0 +1,38 @@
+package myProjects;
+
+public class geniusNorms {
+	public static void main(String[] args) {
+	    System.out.println("Welcome, My name is Ajani");//Print my Welcome Message & Name
+	    System.out.println("My Brothers Info & Community Norms");//Description of this Program
+	   
+	    System.out.println("| | | |");//Marker-Top
+	    System.out.println("V V V V");//Marker-Top
+	    System.out.println("");//Blank Space
+	    
+	    
+	    /*Team Mate 1 - Jordan Stein-Rice */
+	    System.out.println("Teammate 1: Jordan Stein-Rice");//Print my Brother's name
+	    System.out.println("16");//Print my Brother's age
+	    System.out.println("Basketball");//Print My Brother's Passions/Project
+	    System.out.println("");//Blank Space
+	   
+	    
+	    
+	    /*Team Mate 1 - Jordan Stein-Rice  */
+	    System.out.println("Teammate 2: Ajani");//Print My Name
+	    System.out.println("14");//Print My Age
+	    System.out.println("Cooking");//Print My passions/Project
+	    System.out.println("");//Blank Space
+	
+	    /*COMMUNITY NORMS  */
+	    System.out.println("Our Community Norms");//Description 
+	    System.out.println("----------------------");//Divider
+	    System.out.println("1. Giving A Helping Hand ");//Community Norm 1
+	    System.out.println("2. No Cussing");//Community Norm 2
+	    System.out.println("3. Keep Griding");//Community Norm 3
+	    System.out.println("");//Blank Space
+	
+	}  
+}
+
+
